3d316592f9ca27c7ad6554e9f0138e47a9e50580
RussHewgill <RussHewgill@users.noreply.github.com>
2023-10-06 10:46:42 -0700

    maybe fixed images?

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,8 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://gadget3d.ca',
+    image: {
+        remotePatterns: [{ protocol: "https" }],
+        domains: ["raw.githubusercontent.com"],
+      }
 });
