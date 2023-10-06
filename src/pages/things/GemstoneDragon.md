358517e8cbc2c6a60bb092d79996d094c6d30c74
RussHewgill <RussHewgill@users.noreply.github.com>
2023-10-06 09:04:15 -0700

    Astro

---
+++ src/pages/things/GemstoneDragon.md
@@ -0,0 +1,10 @@
+---
+layout: ../../layouts/ThingLayout.astro
+# title: 'Gemstone Dragon'
+# description: 'An Adult Gemstone Dragon'
+# image:
+#     url: ''
+#     alt: 'The full Astro logo.'
+# tags: ["dragon", "cinderwing"]
+uuid: "3119e8b4-c628-4694-87be-ca5b291045d3"
+---
---
+++ src/scripts/fetchthing.ts
@@ -0,0 +1,28 @@
+// import { supabase } from '../supabase';
+
+type ThingProps = {
+  name: string,
+  variation: string,
+  category: string,
+  price: number,
+  // tags: {tag: string}[]
+}
+
+// export const ThingProps = ({name, variation, category, price}: ThingProps) => {
+//   const { data, error } = await supabase
+//     .from('things')
+//     .select();
+  
+// }
+
+// console.error("wat");
+
+// const { data, error } = await supabase
+//     .from('things')
+//     .select();
+
+// if (error) {
+//   console.error(error)
+// }
+
+
